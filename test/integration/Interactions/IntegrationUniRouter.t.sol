4d1102a7e054bae655dabd875c5a965eecae3a43
eliab256 <bordonielia96@gmail.com>
2026-04-04 22:00:03 +0200

    ready for integration test

---
+++ test/integration/Interactions/IntegrationUniRouter.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IntegrationBase} from "./IntegrationBase.t.sol";
+import {IIndex} from "../../../src/Interface/IIndex.sol";
+import {IndexManager} from "../../../src/IndexManager.sol";
+import {Index} from "../../../src/Index.sol";
+import {Router} from "../../../src/Router.sol";
+import {SwapManager} from "../../../src/SwapManager.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IUniversalRouter} from "@uniswap/universal-router/contracts/interfaces/IUniversalRouter.sol";
+
+contract IntegrationUniRouter is IntegrationBase {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    
+}
